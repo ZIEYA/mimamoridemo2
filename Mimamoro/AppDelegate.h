f29bc2df08ffe9559bb56b1dc3d76ab67ae54b16
totyu1 <yyl1713@163.com>
2015-12-02 14:37:36 +0800

    Initial Commit

---
+++ Mimamoro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mimamoro
+//
+//  Created by totyu1 on 2015/12/02.
+//  Copyright © 2015年 totyu1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mimamoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mimamoro
+//
+//  Created by totyu1 on 2015/12/02.
+//  Copyright © 2015年 totyu1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
